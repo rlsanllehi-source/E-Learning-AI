ac8afc002e5ddfe633a69ee902a6960e02349822
rlsanllehi-source <rlsanllehi@gmail.com>
2025-11-24 18:05:26 -0500

    Create Header.tsx

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="bg-blue-600 p-4 text-white">
+      <h1>Cargando la aplicación...</h1>
+    </header>
+  );
+};
