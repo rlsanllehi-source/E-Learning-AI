83f3436563a6b290e36d74bd9e3259c1c0b18435
rlsanllehi-source <rlsanllehi@gmail.com>
2025-11-24 18:37:34 -0500

    Create AboutPage.tsx

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const AboutPage: React.FC = () => {
+  return (
+    <div className="flex-grow p-8 bg-white shadow-lg">
+      <h2 className="text-3xl font-semibold mb-6 text-primary-900">Acerca de Nosotros (Simulado)</h2>
+      <p className="text-lg">Esta es la página de información pública de la empresa.</p>
+      <p className="mt-4 text-gray-600">El contenido real de la página se cargará aquí.</p>
+    </div>
+  );
+};
