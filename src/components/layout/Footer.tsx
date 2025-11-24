c1e52e97450a864da4d4aef179618231bf72ed65
rlsanllehi-source <rlsanllehi@gmail.com>
2025-11-24 18:08:14 -0500

    Create Footer.tsx

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4 text-white text-center">
+      <p>&copy; 2025 E-Learning App</p>
+    </footer>
+  );
+};
